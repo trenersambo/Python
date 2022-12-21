deff75e669356bbd63983d89bc69de46470b3444
da <da@mail.ru>
2022-12-21 19:31:10 +0300

    modify HomeWork26

--- 3_dz/3dz-26.py
+++ 3_dz/3dz-26.py
@@ -9,18 +9,20 @@
 
 # Фибоначчи:    0,1  0,1,1  0,1,1,2   0,1,1,2,3    0,1,1,2,3,5   0,1,1,2,3,5,8   0,1,1,2,3,5,8,13   0,1,1,2,3,5,8,13,21
 # число юзера:   1      2       3       4               5               6               7                   8
-# F n = F n − 1 + F n − 2
+# для положительных: F n = F n − 1 + F n − 2
+# для отрицательных: f(n)= f(n+2) - f(n+1)
 
 def calcFibi(num):
-    #print(num, type(num))
+    #if для отрицательного числа (crutch)
+    if (num < -1):
+        num = -num
 
-    arr = [0,1]     # стратовый массиа; (по умолч.) при вводе 1 = [0,1]
+    arr = [0,1]     # стратовый массив; (по умолч.) при вводе 1 = [0,1]
 
     count = 1
 
-
     while(count<num):
-        if num == 1:
+        if num == 1:    #исключение для введенной 1
             print(arr)
             break
 
@@ -28,12 +30,39 @@ def calcFibi(num):
             acc = arr[len(arr)-1] + arr[len(arr)-2]
             arr.append(acc)
 
-            print(arr)
-
             count +=1
 
+    print(f'Числа Фибоначчи для {num} = {arr}')
+    return  arr
 
 
+# 2. ф-ция для обработки массива с числами Фибоначчи для Отрицательных чисел
+def calcFibiNeg(negNum):
+    #if для отрицательного числа (crutch)
+    if (negNum > -1):
+        negNum = -negNum
 
+    arrNeg = [0,1]  # стратовое знач. для негаФибоначчи: при вводе -1 = [0,1]
+
+    count = -1
+
+    while(count > negNum):
+        #print(f'count={count} , negNum={negNum}')
+        if negNum == -1:
+            print(arrNeg)    #исключение для введенной -1 --> [0,1]
+
+        if(negNum < -1):
+
+            acc = arrNeg[len(arrNeg)-2] - arrNeg[len(arrNeg)-1]
+            arrNeg.append(acc)
+
+            count -=1   # -2 -3 -4 -5 ... пока не достигнет negNum
+
+    print(f'Числа Фибоначчи для {negNum} = {arrNeg}')
+
+
+# вызов методов/функций
 numFibi = int(input('введи цифру для расчета числа Фибоначчи (>=1): '))
 calcFibi(numFibi)
+
+calcFibiNeg(numFibi)
